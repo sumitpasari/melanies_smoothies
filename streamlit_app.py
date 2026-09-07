516198fb5fe6fdb203b3791edbb7801bd4a6ac1b
sumitpasari <sumit.pasari@gmail.com>
2026-09-07 21:53:26 +0530

    Modify fruit options selection and display
    Updated the data selection to include 'SEARCH_ON' and converted to a pandas DataFrame for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import os
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(f":cup_with_straw:  Custom Smoothie Order Form :cup_with_straw: ")
@@ -17,8 +18,10 @@ st.write("The name on your smoothie will be: ",name_on_order)
 cnx=st.connection("snowflake")
 session=cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True)   
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect("Choose upto 5 fruits: ", my_dataframe)
 
